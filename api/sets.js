c84518df0bc5668e1f5021fab57d06c0ea87d476
aMikeF <emfurgalski@gmail.com>
2026-02-17 18:33:11 +0100

    debug: check env var

--- api/sets.js
+++ api/sets.js
@@ -1,21 +1,6 @@
 export default async function handler(req, res) {
-  try {
-    const response = await fetch(
-      'https://api.pokemontcg.io/v2/sets?orderBy=-releaseDate',
-      { headers: { 'X-Api-Key': process.env.TCG_API_KEY } }
-    )
-
-    if (!response.ok) {
-      throw new Error(`pokemontcg API error: ${response.status}`)
-    }
-
-    const data = await response.json()
-
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.status(200).json(data)
-
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: err.message })
-  }
+  res.status(200).json({
+    key_exists: !!process.env.TCG_API_KEY,
+    key_preview: process.env.TCG_API_KEY?.slice(0, 8) + '...'
+  })
 }
